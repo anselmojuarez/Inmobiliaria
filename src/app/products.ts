653e31e3a2432f773f6f4b0c6545e41e134ded0f
anselmo <166w0348@zongolica.tecnm.mx>
2020-05-27 12:37:58 -0500

    Inmobiliaria

---
+++ src/app/products.ts
@@ -0,0 +1,52 @@
+export const  products =[
+{
+    name:'Elegante clasicismo',
+    price: 800.000,
+    modelo: 'Modelo 1',
+    description:'La vigencia de estilos cuyo origen se remonta a tiempos muy alejados del actual sólo refleja su belleza y singularidad, y es precisamente este par de cualidades la que logran que hoy en día se siga imitando el estilo clásico en la construcción de una casa.',
+    imagen: "/assets/img/modelo1.jpg"
+},
+{
+    name:'Moderna y abierta al jardín',
+    price: 180.000,
+    modelo: 'Modelo 2',
+    description:'Aquí la madera es la máxima protagonista de esta fachada puesto que el resto de los elementos tiene como objetivos primordiales, ser netamente funcionales y resaltar a este material que la naturaleza brinda.',
+    imagen: "/assets/img/modelo2.jpg"
+},
+{
+    name:'Belleza colonial',
+    price: 180.000,
+    modelo: 'Modelo 3',
+    description:'Este segundo estilo responde a un diseño que busca una sencillez capaz de comunicar a primera vista un acogimiento sin igual. Es la firma Alessandro Muscolini & Patrizia Zagaglia Architetti quien realiza esta construcción digna de representar lo colonial, pues conjunta varios elementos sumamente característicos de dicho estilo como son las pequeñas torres superiores y ese remate en pico del techo principal, así como la crudeza de lo muros y la exacta forma de las ventanas hechas de madera',
+    imagen: "/assets/img/modelo3.jpg"
+},
+{
+    name:'Sencillez minimalista',
+    price: 180.000,
+    detail:'Todos nuestros modelos,son creados con materiales de lujo.cada una tiene sus particularidades y aunque todas siguen una misma linea constructiva,tienen diferencias notables',
+    modelo: 'Modelo 4',
+    description:'Bajo la sentencia de que menos es más es como parte Excelencia en Diseño para la construcción de esta casa que juega en su fachada con sencillos módulos, los cuales adquieren singularidad al ser recubiertos de ciertas partes por adoquines en tonos cafés. ',
+    imagen: "/assets/img/modelo4.jpg"
+},
+{
+    name:'Bienvenida mediterránea',
+    price: 180.000,
+    modelo: 'Modelo 5',
+    description:'Pretender crear una fachada con esencia del mediterráneo implica recurrir estrictamente a ciertos elementos como la luminosidad tonal y la naturalidad de los materiales. Tomando a estos aspectos como ejes principales de esta construcción es que Mikkael Kreis Architects construye esta fachada exterior donde el blanco se encarga de irradiar luz y sencillez, mientras la madera, dedicada a ser estructura de las múltiples ventanas y el techo, luce con los mínimos retoques.',
+    imagen: "/assets/img/modelo5.jpg"
+},
+{
+    name:'Eclecticismo total',
+    price: 180.000,
+    modelo: 'Modelo 6',
+    description:'No hay mejor mezcla que la que logra lo ecléctico y esto lo saben muy bien los profesionales que componen a Ah Arquitectos Hernandez, por ello no dudan en recurrir a este estilo para llevar a cabo su idea que consiste en fusionar características decorativas de lo colonial y lo mexicano contemporáneo.',
+    imagen: "/assets/img/modelo6.jpg"
+},
+{
+    name:'Representación moderna',
+    price: 180.000,
+    modelo: 'Modelo 7',
+    description:'Aquí otro de los favoritos en el diseño de casas, el estilo moderno. M67 Architekten junta la forma, el color y lo materiales que se han vuelto un ícono de la modernidad y crea esta fachada digna de admirar.',
+    imagen: "/assets/img/modelo7.jpg"
+}
+];
